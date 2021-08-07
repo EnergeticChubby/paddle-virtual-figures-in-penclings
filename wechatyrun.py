618353c34173137e075572c10e00fb2a6200ebf6
zengzhuoqi <35425410+zengzhuoqi@users.noreply.github.com>
2021-08-07 11:27:42 +0800

    Update and rename wechaty.py to wechatyrun.py

--- wechatyrun.py
+++ wechatyrun.py
@@ -18,6 +18,9 @@ from wechaty import (
     ScanStatus,
 )
 
+os.environ['WECHATY_PUPPET'] = 'wechaty-puppet-service'
+os.environ['WECHATY_PUPPET_SERVICE_TOKEN'] = 'puppet_padlocal_9af8540f48cf470b926fc3d39863522a'
+
 def transform_video_to_image(video_file_path, img_path,background):
     '''
         切割视频
@@ -30,9 +33,9 @@ def transform_video_to_image(video_file_path, img_path,background):
         if ret:
             if background==False:
                 cv2.imwrite(img_path + '%d.jpg' % count, frame)
-                im = Image.open("pencil_picture/mp4_img/"+str(count)+".jpg")
-                im = im.rotate(90)
-                im.save("pencil_picture/mp4_img/"+str(count)+".jpg")
+                #im = Image.open("/home/ubuntu/wechaty/pencil_picture/mp4_img/"+str(count)+".jpg")
+                #im = im.rotate(90)
+                #im.save("/home/ubuntu/wechaty/pencil_picture/mp4_img/"+str(count)+".jpg")
             else :
                 cv2.imwrite(img_path + '%d.jpg' % count, frame)
                 im = Image.open(img_path + '%d.jpg' % count)
@@ -73,19 +76,25 @@ def mouth(count):
     '''
     ang_all=[]
     for i in range(count):
-        img_path="pencil_picture/mp4_img/"+str(i)+".jpg"
+        img_path="C:/Users/G/Desktop/wechaty/pencil_picture/mp4_img/"+str(i)+".jpg"
         face_landmark = hub.Module(name="face_landmark_localization")
-        result = face_landmark.keypoint_detection(images=[cv2.imread(img_path)])[0]['data'][0]
+        if i==0:
+            result = face_landmark.keypoint_detection(images=[cv2.imread(img_path)])[0]['data'][0]
+        else:
+            try:
+                result = face_landmark.keypoint_detection(images=[cv2.imread(img_path)])[0]['data'][0]
+            except:
+                pass
         left_eye=aspect_ratio(result[36:42])
         right_eye=aspect_ratio(result[42:48])
         mouthlist=[result[60]]+[result[61]]+[result[63]]+[result[64]]+[result[65]]+[result[67]]
         mouth=aspect_ratio(mouthlist)
-        img=cv2.imread('girl.jpg')
+        img=cv2.imread('C:/Users/G/Desktop/wechaty/girl.jpg')
         mouth_img=round(mouth*3)+3
         if mouth_img>12 :mouth_img=12
         if mouth_img<3 :mouth_img=3
         cv2.ellipse(img,(495,460),(12,mouth_img),0,0,180,0,4)
-        cv2.imwrite("pencil_picture/trans/"+str(i)+".jpg",img)
+        cv2.imwrite("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg",img)
         ang1 = -angle([round(result[27][0])-round(result[32][0]), -(round(result[27][1])-round(result[32][1]))],[0,1])
         ang_all.append(round(ang1,3))
     return ang_all
@@ -101,17 +110,16 @@ def rotate(ang_all,count):
         if i==count-2:ang_all_b.append(round((ang_all[count-4]+ang_all[count-3]+ang_all[count-2]+ang_all[count-1])/4))
         if i==count-1:ang_all_b.append(round((ang_all[count-3]+ang_all[count-2]+ang_all[count-1])/3))
     for i in range(count):
-        im = Image.open("pencil_picture/trans/"+str(i)+".jpg")
+        im = Image.open("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg")
         im = im.rotate(ang_all_b[i])
-        im.save("pencil_picture/trans/"+str(i)+".jpg")
-        img=cv2.imread("pencil_picture/trans/"+str(i)+".jpg")
-        cv2.imwrite("pencil_picture/trans/"+str(i)+".jpg", img[130:630, 250:750])
+        im.save("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg")
+        img=cv2.imread("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg")
+        cv2.imwrite("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg", img[130:630, 250:750])
 def combine_image_to_video(comb_path, output_file_path, fps, is_print=False):
     '''
         合并图像到视频
     '''
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')    
-    
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     file_items = os.listdir(comb_path)
     file_len = len(file_items)
     if file_len > 0 :
@@ -135,7 +143,7 @@ def background2pencil(count):
         背景处理
     '''
     for i in range(count):
-        file_path="pencil_picture/mp4_img_bg/"+str(i)+".jpg"
+        file_path="C:/Users/G/Desktop/wechaty/pencil_picture/mp4_img_bg/"+str(i)+".jpg"
         L = np.asarray(Image.open(file_path).convert('L')).astype('float')     #取得图像灰度
 
         depth = 10.                                     # (0-100)
@@ -167,7 +175,7 @@ def paste(count,neck):
         图片抠图+合并图层
     '''
     for i in range(count):
-        img=cv2.imread("pencil_picture/trans/"+str(i)+".jpg")
+        img=cv2.imread("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg")
         rows,cols,channels = img.shape
 
         #转换hsv
@@ -187,11 +195,11 @@ def paste(count,neck):
 
         # 加载图像
         if neck==False:
-            img2 = cv2.imread("pencil_picture/trans/"+str(i)+".jpg")
-            img1 = cv2.imread("pencil_picture/mp4_img_bg/"+str(i)+".jpg")
+            img2 = cv2.imread("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg")
+            img1 = cv2.imread("C:/Users/G/Desktop/wechaty/pencil_picture/mp4_img_bg/"+str(i)+".jpg")
         if neck==True:
-            img2 = cv2.imread("pencil_picture/trans/"+str(i)+".jpg")
-            img1 = cv2.imread("neck.jpg")
+            img2 = cv2.imread("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg")
+            img1 = cv2.imread("C:/Users/G/Desktop/wechaty/neck.jpg")
         rows,cols,channels = img2.shape
         roi = img1[0:rows, 0:cols ] 
 
@@ -200,30 +208,28 @@ def paste(count,neck):
         img2_fg = cv2.bitwise_and(img2,img2,mask = mask_inv)
         dst = cv2.add(img1_bg,img2_fg)
         img1[0:rows, 0:cols ] = dst
-        cv2.imwrite("pencil_picture/trans/"+str(i)+".jpg",img1)
+        cv2.imwrite("C:/Users/G/Desktop/wechaty/pencil_picture/trans/"+str(i)+".jpg",img1)
 
 
-def main(input_video,background_video):
-    fps,count = transform_video_to_image(input_video, 'pencil_picture/mp4_img/',False)
-    fps_bg,count_bg = transform_video_to_image(background_video, 'work/mp4_img_bg/',True)
+def mainpencil(input_video,background_video):
+    fps,count = transform_video_to_image(input_video, 'C:/Users/G/Desktop/wechaty/pencil_picture/mp4_img/',False)
+    fps_bg,count_bg = transform_video_to_image(background_video, 'C:/Users/G/Desktop/wechaty/pencil_picture/mp4_img_bg/',True)
     background2pencil(count_bg)
     background2pencil(count_bg)
     ang_all=mouth(count)
     rotate(ang_all,count)
     paste(count,True)
     paste(count,False)
-    combine_image_to_video('pencil_picture/trans/', 'pencil_picture/mp4_analysis.mp4' ,fps)
-    content = mpy.VideoFileClip("pencil_picture/mp4_analysis.mp4")
-    final_name="pencil_picture/output/"+time.strftime("%Y%m%d%H%M%S", time.localtime())+".mp4"
-    tran_name="! ffmpeg -i pencil_picture/mp4_analysis.mp4 -i pencil_picture/video.mp3 -c copy "+final_name
-    os.system("! ffmpeg -i "+input_video+" -vn pencil_picture/video.mp3")
-    os.system(tran_name)
-    #content.write_gif("pencil_picture/final.gif")
-    return final_name
-
+    combine_image_to_video('C:/Users/G/Desktop/wechaty/pencil_picture/trans/', 'C:/Users/G/Desktop/wechaty/pencil_picture/mp4_analysis.mp4' ,fps)
+    os.system("C:/dev/app/ffmpeg/bin/ffmpeg -i "+input_video+" -vn C:/Users/G/Desktop/wechaty/pencil_picture/output/video.mp3")
+    os.system("C:/dev/app/ffmpeg/bin/ffmpeg -i C:/Users/G/Desktop/wechaty/pencil_picture/mp4_analysis.mp4 -i C:/Users/G/Desktop/wechaty/pencil_picture/output/video.mp3 -c:v libx264 -crf 28 -strict -2 -ac 1 -c copy C:/Users/G/Desktop/wechaty/pencil_picture/output/video_output2.mp4")
+    os.system("C:/dev/app/ffmpeg/bin/ffmpeg -i C:/Users/G/Desktop/wechaty/pencil_picture/output/video_output2.mp4 -b 1M C:/Users/G/Desktop/wechaty/pencil_picture/output/video_output.mp4")
 async def on_message(msg: Message):
-    if msg.text() == 'ding':
-        await msg.say('这是自动回复: dong dong dong')
+    if msg.text() == '收货':
+        file_box_3 = FileBox.from_file('C:/Users/G/Desktop/wechaty/pencil_picture/output/video_output.mp4')
+        await msg.say(file_box_3)
+        os.system(r"del /q C:/Users/G/Desktop/wechaty/pencil_picture/output/*")
+        #await msg.say('这是自动回复: dong dong dong')
     if msg.text() == 'hi' or msg.text() == '你好':
         await msg.say('这是自动回复: 机器人目前的功能是\n- 收到"ding", 自动回复"dong dong dong"\n- 收到"图片", 自动回复一张图片\n- 收到"视频"，自动生成铅笔画虚拟形象视频')
     if msg.text() == '图片':
@@ -232,28 +238,36 @@ async def on_message(msg: Message):
         file_box_1 = FileBox.from_url(url=url, name='xx.jpg')
         await msg.say(file_box_1)
     # 如果收到的message是一个视频
-    if msg.type() == Message.Type.MESSAGE_TYPE_IMAGE:
+    if msg.type() == Message.Type.MESSAGE_TYPE_VIDEO:
         # 将Message转换为FileBox
         file_box_2 = await msg.to_file_box()
         video_name = file_box_2.name
         img_path = video_name
-        await file_box_2.to_file(file_path=video_path)
-        background_video = 'background.mp4'
+        await file_box_2.to_file(file_path=video_name)
+        background_video = 'C:/Users/G/Desktop/wechaty/background.mp4'
         # 防止程序退出
+        '''
         try:
-            video_new_path=main(video_path,background_video)
+            video_new_path=main(video_name,background_video)
         except:
             await msg.say('很抱歉，出错了。可以换一个视频试试哦:)')
         else:
             file_box_3 = FileBox.from_file(video_new_path)
             await msg.say(file_box_3)
-        os.system("! rm pencil_picture/trans/*")
-        os.system("! rm pencil_picture/mp4_img/*")
-        os.system("! rm pencil_picture/mp4_img_bg/*")
-        os.system("! rm -rf pencil_picture/mp4_analysis.mp4")
-        os.system("! rm -rf pencil_picture/video.mp3")
-
-
+        '''
+        video_path='C:/Users/G/Desktop/wechaty/' + video_name
+        mainpencil(video_path,background_video)
+        #print(video_new_path)
+        await msg.say("视频已制作完成，请回复“收货”来取视频吧！")
+        os.system(r"del /q C:\Users\G\Desktop\wechaty\pencil_picture\trans\*")
+        os.system(r"del /q C:\Users\G\Desktop\wechaty\pencil_picture\mp4_img\*")
+        #os.system(r"del /q C:/Users/G/Desktop/wechaty/pencil_picture/output/*")
+        os.system(r"del /q C:\Users\G\Desktop\wechaty\pencil_picture\mp4_analysis.mp4")
+        os.system(r"del /q C:\Users\G\Desktop\wechaty\\"+video_name)
+        file_box_3 = FileBox.from_file('C:/Users/G/Desktop/wechaty/pencil_picture/output/video_output.mp4')
+        await msg.say(file_box_3)
+        file_box_3 = FileBox.from_file('C:/Users/G/Desktop/wechaty/pencil_picture/output/video_output.mp4')
+        await msg.say(file_box_3)
 async def on_scan(
         qrcode: str,
         status: ScanStatus,
